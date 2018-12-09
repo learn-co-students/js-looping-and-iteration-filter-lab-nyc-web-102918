02096449f0f9dc3d0ba8f0187e4dd3621d48f9e0
ruthmesfun <ruth.mesfun@gmail.com>
2018-12-09 10:17:41 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution in this file
+let findMatching = (drivers, name) => {
+    return drivers.filter(x => x.toLowerCase() === name.toLowerCase());
+}
+
+let fuzzyMatch = (drivers, partial) => {
+    return drivers.filter(x => x.slice(0,partial.length) === partial);
+}
+
+let matchName = (drivers, name) => {
+   return drivers.filter(driver => driver.name === name);
+}
