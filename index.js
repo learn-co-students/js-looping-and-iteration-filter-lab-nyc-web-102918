951f38398462a90415adf1b3304170cba0b89ba2
vibhu mahendru <vibhumahendru@vibhus-MacBook-Pro.local>
2018-12-10 11:53:31 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution in this file
+// Code your solution in this
+
+function findMatching(drivers, string) {
+  return drivers.filter(function (str){
+    return str.toUpperCase() === string.toUpperCase();})
+}
+
+function fuzzyMatch(drivers, string) {
+  let lengthName = string.length;
+
+  return drivers.filter(function (str){
+    return str.slice(0, lengthName) === string;
+  })
+}
+
+function matchName(drivers, string) {
+return drivers.filter(function (element){
+  return element.name === string;
+})
+}
